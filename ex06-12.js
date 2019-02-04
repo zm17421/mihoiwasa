a6a115c941282f12b71849cc64b093c9f168c776
zm17421 <zm17421@josai.ac.jp>
2019-02-04 07:54:58 -0500

    message

---
+++ ex06-12.js
@@ -0,0 +1,6 @@
+function ThreeSecChange() {
+  setInterval("imgchange12()",3000); }
+
+function imgchange12() {
+  document.getElementById('logo2').src = './image/四送会.JPG';
+}
---
+++ ex06-9.js
@@ -0,0 +1,6 @@
+function txtchange1() {
+document.getElementById('txt1').innerHTML = 'ナンバーワン!';
+}
+function imgchange1(){
+  document.getElementById('logo').src = './image/駅伝２.JPG';
+}
